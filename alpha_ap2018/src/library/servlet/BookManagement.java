cdf652f2e7804c584bdd9ec4cf95273c2d448c04
ken-newc <ken-newc@192.168.11.15>
2018-06-21 15:40:27 +0900

    ファイル構成の修正

--- alpha_ap2018/src/library/servlet/BookManagement.java
+++ alpha_ap2018/src/library/servlet/BookManagement.java
@@ -1,11 +1,19 @@
 package library.servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import library.bean.BookBean;
+import library.dao.BookDAO;
+import library.dao.SubjectDAO;
 
 /**
  * Servlet implementation class BookManagement
@@ -13,7 +21,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/BookManagement")
 public class BookManagement extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,7 +35,33 @@ public class BookManagement extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		String button = request.getParameter("button");
+		if(button.equals("書籍の追加")) {
+			ServletContext context = getServletContext();
+			RequestDispatcher rd = context.getRequestDispatcher("/AddBook.jsp");
+			rd.forward(request, response);
+		}else if(button.equals("詳細")) {
+			BookBean bbn = new BookBean();
+			String subName="";
+			try {
+				BookDAO bookDAO = new BookDAO();
+				bbn = bookDAO.getBookBean(request.getParameter("label"));
+				SubjectDAO subDAO = new SubjectDAO();
+				subName=subDAO.getSubjectName(bbn.getSubjectId());
+			} catch (SQLException e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+			}
+			request.setAttribute("bookData",bbn);
+			request.setAttribute("subName", subName);
+
+			ServletContext context = getServletContext();
+			RequestDispatcher rd = context.getRequestDispatcher("/BookManagement.jsp");
+			rd.forward(request, response);
+		}else {
+			System.out.println("error");
+		}
 	}
 
 	/**
@@ -35,7 +69,43 @@ public class BookManagement extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		request.setCharacterEncoding("UTF-8");
 
+		String button = request.getParameter("button");
+		if(button.equals("詳細")) {
+			doGet(request,response);
+		} else if(button.equals("更新")){
+			try {
+				BookDAO bookDAO = new BookDAO();
+				bookDAO.renewBookData(request.getParameter("label"),request.getParameter("title"),request.getParameter("author"),
+					request.getParameter("publisher"),request.getParameter("publicationYear"),
+					request.getParameter("stockNum"),request.getParameter("subName"),request.getParameter("imagePath"));
+			} catch (SQLException e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+			}
+			doGet(request,response);
+		} else if(button.equals("削除")) {
+			try {
+				BookDAO bookDAO = new BookDAO();
+				bookDAO.deleteBookData(request.getParameter("label"));
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		} else if(button.equals("追加")){
+			try {
+				BookDAO bookDAO = new BookDAO();
+				bookDAO.addBookData(request.getParameter("label"),request.getParameter("title"),request.getParameter("author"),
+						request.getParameter("publisher"),request.getParameter("publicationYear"),
+						request.getParameter("stockNum"),request.getParameter("subName"),request.getParameter("imagePath"));
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}else{
+			System.out.println("ボタン判定エラー");
+		}
+		ServletContext context = getServletContext();
+		RequestDispatcher rd = context.getRequestDispatcher("/BookSearch.jsp");
+		rd.forward(request, response);
+	}
 }
--- alpha_ap2018/src/library/servlet/BookSearch.java
+++ alpha_ap2018/src/library/servlet/BookSearch.java
@@ -1,11 +1,20 @@
 package library.servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import library.bean.BookBean;
+import library.dao.BookDAO;
 
 /**
  * Servlet implementation class BookSearch
@@ -13,7 +22,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/BookSearch")
 public class BookSearch extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,7 +36,23 @@ public class BookSearch extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		// ユーザー一覧格納用
+		List<BookBean> bookList = new ArrayList<BookBean>();
+		// ユーザー一覧を取得する
+		try {
+			BookDAO bookDAO = new BookDAO();
+			bookList = bookDAO.getAllBookData();
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+		request.setAttribute("bookList", bookList);
+		request.setAttribute("message", "検索条件を入力してください");
+		ServletContext context = getServletContext();
+		RequestDispatcher rd = context.getRequestDispatcher("/BookSearch.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
@@ -35,7 +60,34 @@ public class BookSearch extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
 
+		request.setCharacterEncoding("UTF-8");
+
+		// 遷移先JSPを格納
+		String jsp = "/BookSearch.jsp";
+		String btn = request.getParameter("button");
+
+
+		try {
+			BookDAO bookDAO = new BookDAO();
+			List<BookBean> bookList = new ArrayList<BookBean>();
+			bookList = bookDAO.getBookDataByInput(request.getParameter("title"),request.getParameter("author"),
+						request.getParameter("publisher"),request.getParameter("subject"));
+			request.setAttribute("bookList", bookList);
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		if(!btn.equals("RESET")) {
+			request.setAttribute("title",request.getParameter("title"));
+			request.setAttribute("author",request.getParameter("author"));
+			request.setAttribute("publisher",request.getParameter("publisher"));
+			request.setAttribute("subject",request.getParameter("subject"));
+		}
+
+		// JSPに遷移
+		ServletContext context = getServletContext();
+		RequestDispatcher rd = context.getRequestDispatcher(jsp); // 転送先のURL
+		rd.forward(request, response);
+	}
 }
